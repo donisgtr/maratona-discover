21fdf76fa80668f11810ca35fbc1531d65d43969
donissousa <donisgtr@gmail.com>
2021-02-05 09:02:08 -0300

    aula 03

---
+++ scripts.js
@@ -0,0 +1,93 @@
+const Modal = {
+    open () {
+      // abrir modal
+      // Adicionar a class active ao modal
+      document
+          .querySelector('.modal-overlay')
+          .classList
+          .add('active');
+    },
+    close () {
+      // fechar o modal
+      // remover class active do modal
+      document
+          .querySelector('.modal-overlay')
+          .classList
+          .remove('active');
+    }
+}
+
+const transactions = [
+
+    {
+    id: 1,
+    description: 'Luz',
+    amount: -50000,
+    date: '23/01/2021',
+    },
+
+    {
+    id: 2,
+    description: 'Website',
+    amount: -500000,
+    date: '23/01/2021',
+    },
+
+    {
+    id: 3,
+    description: 'Internet',
+    amount: -50000,
+    date: '23/01/2021',
+    },
+
+]
+
+const Transaction = {
+    incomes() {
+        // somar as entradas
+    },
+    expenses() {
+        // somar as saidas
+    },
+    total () {
+        // entradas - saidas
+    }
+}
+
+const DOM = {
+
+    transactionsContainer: document.querySelector("#data-table tbody"),
+    
+    addTransation(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+        DOM.transactionsContainer.appendChild(tr);
+    },
+
+    innerHTMLTransaction(transaction) {
+
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+        const amount = Utils.formatCurrency(transaction.amount);
+        
+        const html = `
+            <td class="description">${transaction.description}</td>
+            <td class="expense">${transaction.amount}</td>
+            <td class="data">${transaction.date}</td>
+            <td>
+                <img src="assets/minus.svg" alt="Remover Transação" />
+            </td> `
+
+        return html
+    }
+}
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+    }
+}
+
+transactions.forEach(function (transaction) {
+    DOM.addTransation(transaction);
+});
